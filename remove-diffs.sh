12dfc99e62b420fde21b0ceb83bf04a34934ccfd
Gustavo Harff <gustavo.harff@gmail.com>
2022-10-14 19:24:35 -0300

    Remove releases diffs

---
+++ remove-diffs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+version=$0
+
+find ./diffs -name "$version..*.diff" -delete
+find ./diffs -name "*..$version.diff" -delete
+
+git add .
+git commit -m "Remove releases diffs"
+git push origin diffs
+
+git push --delete origin "release/$version"
+git tag -d version/$version
