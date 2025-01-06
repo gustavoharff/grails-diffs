63bf297752548640a4a0b90e70f24e4106a2481c
Anka <runner@Mac-1736198015154.local>
2025-01-06 21:24:37 +0000

    Release 6.2.3-rest-api-plugin-plugin

---
+++ myplugin/grails-app/init/myplugin/Application.groovy
@@ -0,0 +1,13 @@
+package myplugin
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+import groovy.transform.CompileStatic
+import grails.plugins.metadata.*
+
+@PluginSource
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
