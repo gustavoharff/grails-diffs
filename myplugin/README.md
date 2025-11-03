3614f6b97157425822d8aa1fd979166d20d141e9
Anka <runner@iad20-gt1023-16525313-f64e-43ca-a3f5-761dd29eb19f-3E80548E7F40.local>
2025-11-03 17:13:33 +0000

    Release 7.0.1-rest-api-plugin-plugin

---
+++ myplugin/README.md
@@ -0,0 +1,11 @@
+## Grails 7.0.1 Documentation
+
+- [User Guide](https://grails.apache.org/docs/7.0.1/guide/index.html)
+- [API Reference](https://grails.apache.org/docs/7.0.1/api/index.html)
+- [Grails Guides](https://guides.grails.org/index.html)
+---
+
+## Feature spring-boot-devtools documentation
+
+- [Grails SpringBoot Developer Tools documentation](https://docs.spring.io/spring-boot/reference/using/devtools.html)
+
---
+++ myplugin/src/main/groovy/myplugin/MypluginGrailsPlugin.groovy
@@ -0,0 +1,68 @@
+package myplugin
+
+import grails.plugins.*
+
+class MypluginGrailsPlugin extends Plugin {
+
+    // the version or versions of Grails the plugin is designed for
+       def grailsVersion = "7.0.1  > *"
+       // resources that are excluded from plugin packaging
+       def pluginExcludes = [
+           "grails-app/views/error.gsp"
+       ]
+
+       // TODO Fill in these fields
+       def title = "Myplugin" // Headline display name of the plugin
+       def author = "Your name"
+       def authorEmail = ""
+       def description = '''\
+   Brief summary/description of the plugin.
+   '''
+   // URL to the plugin's documentation
+       def documentation = "https://grails-plugins.github.io/Myplugin/"
+
+       // Extra (optional) plugin metadata
+
+       // License: one of 'APACHE', 'GPL2', 'GPL3'
+   //    def license = "APACHE"
+
+       // Details of company behind the plugin (if there is one)
+   //    def organization = [ name: "My Company", url: "https://www.my-company.com/" ]
+
+       // Any additional developers beyond the author specified above.
+   //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+       // Location of the plugin's issue tracker.
+   //    def issueManagement = [ system: "GitHub", url: "https://github.com/apache/Myplugin/issues" ]
+
+       // Online location of the plugin's browseable source code.
+   //    def scm = [ url: "https://github.com/apache/Myplugin" ]
+
+       Closure doWithSpring() { {->
+               // TODO Implement runtime spring config (optional)
+           }
+       }
+
+       void doWithDynamicMethods() {
+           // TODO Implement registering dynamic methods to classes (optional)
+       }
+
+       void doWithApplicationContext() {
+           // TODO Implement post initialization spring config (optional)
+       }
+
+       void onChange(Map<String, Object> event) {
+           // TODO Implement code that is executed when any artefact that this plugin is
+           // watching is modified and reloaded. The event contains: event.source,
+           // event.application, event.manager, event.ctx, and event.plugin.
+       }
+
+       void onConfigChange(Map<String, Object> event) {
+           // TODO Implement code that is executed when the project configuration changes.
+           // The event is the same as for 'onChange'.
+       }
+
+       void onShutdown(Map<String, Object> event) {
+           // TODO Implement code that is executed when the application shuts down (optional)
+       }
+}
